f3ce00858d59b78cb5a3102fca8c1c268260db52
ashish0414 <ashishkrsingh91@gmail.com>
2026-02-09 19:09:49 +0530

    Addes api reqres details

--- tests/GetUsersDetails.spec.js
+++ tests/GetUsersDetails.spec.js
@@ -2,9 +2,9 @@ import test, { expect } from "playwright/test";
 
 test.describe("Get User Details", () => {
     test('GET Users Details', async ({ request }) => {
-        const response = await request.get("/api/users/");
+        const response = await request.get("/api/users?page=2");
         //const responseBody = JSON.parse(await response.text());
-        console.log(await response.text());
+        console.log(await response.json());
         expect(response.status()).toBe(200);
         //expect(responseBody.data.id).toBe(2);
 
